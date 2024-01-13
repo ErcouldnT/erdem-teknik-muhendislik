a37c02d17cfeccbf4aa09e9015f35d306587d8a0
snnrdm <156531695+snnrdm@users.noreply.github.com>
2024-01-13 22:21:55 +0300

    Update +page.svelte

--- src/routes/randevu/+page.svelte
+++ src/routes/randevu/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Randevu al</h1>
-<p>Buraya bakarlar</p>
+<h1>Bizimle İletişime Geç!</h1>
+
+İletişim Bilgileri
+Adres: Ulubatlı Hasan Mahallesi Samsun Caddesi No: 47 Sincan / ANKARA
+Telefon: 0 (506) 392 52 35
